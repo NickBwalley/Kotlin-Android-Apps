edddd644914500861c1ca49579802cd15319b9fa
NickBwalley <nickbwalley@gmail.com>
2022-10-21 12:42:35 +0300

    implicit vs explicit intents

--- Intents/app/src/main/java/com/example/intents/PhotoActivity.kt
+++ Intents/app/src/main/java/com/example/intents/PhotoActivity.kt
@@ -2,10 +2,31 @@ package com.example.intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class PhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
+
+        var btnLaunchMain : Button = findViewById(R.id.btnBack)
+
+        btnLaunchMain.setOnClickListener(
+            //Explicit intent
+            // Move to another SPECIFIC APP replace package name with yours
+            var packageName = "com.example.intents"
+
+            var launchApp: Intent? = packageManager.getLaunchIntentForPackage(packageName)
+
+            // confirm if ther eis an app with that package name, if not tell user APP NOT FOUND
+            if(launchApp != null){
+                startActivity(launchApp)
+            } else{
+                Toast.makeText(this, "APP NOT FOUND!", Toast.LENGTH_LONG).show()
+            }
+
+        )
+
     }
 }
