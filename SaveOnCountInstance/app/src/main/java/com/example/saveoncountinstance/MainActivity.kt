ca3f5b309e973c2601b2686f2b4537f8530629df
NickBwalley <nickbwalley@gmail.com>
2022-10-24 23:19:24 +0300

    completed part one increment and decrement

--- SaveOnCountInstance/app/src/main/java/com/example/saveoncountinstance/MainActivity.kt
+++ SaveOnCountInstance/app/src/main/java/com/example/saveoncountinstance/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.saveoncountinstance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var incrementNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun incrementValue(view: View) {
+        // increment value
+        incrementNumber++
+        // locate the textView and update tne number
+        val textView = findViewById<TextView>(R.id.show_count)
+        textView.text = incrementNumber.toString()
+
+    }
+    fun decrementValue(view: View) {
+        // increment value
+        incrementNumber--
+        // locate the textView and update tne number
+        val textView = findViewById<TextView>(R.id.show_count)
+        textView.text = incrementNumber.toString()
     }
 }
