f2accc3cbe1895ca7edef56086ebc648e871f6f8
NickBwalley <nickbwalley@gmail.com>
2022-10-25 13:08:21 +0300

    second activity completed

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // inflate the UI -- is transforming the XML layout into java/kotlin objects to work with
         setContentView(R.layout.activity_main)
+
+        // DISPLAY MESSAGE BACK TO THE MAIN ACTIVITY VIEW.
+        val message = intent.getStringExtra("SEND_VALUE_BACK")
+
+        var textView: TextView = findViewById(R.id.text_message2)
+
+        textView.text = message
+        ///////////////////////////////////////////////////////////
 
         val nMessageEditText: EditText  = findViewById(R.id.editText_send)
 
--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -3,6 +3,8 @@ package com.example.twoactivities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -15,6 +17,22 @@ class SecondActivity : AppCompatActivity() {
         var textView: TextView = findViewById(R.id.text_message)
 
         textView.text = message
+
+        // SEND ACTIVITY FROM SECONDACTIVITY TO MAINACTIVITY
+        val nMessageEditText: EditText = findViewById(R.id.editText_send2)
+
+        val buttonSend : Button = findViewById(R.id.button_send2)
+
+        buttonSend.setOnClickListener{
+            val secondActivityIntent = Intent(this, MainActivity::class.java)
+
+
+            var message = nMessageEditText.text.toString().trim()
+            secondActivityIntent.putExtra("SEND_VALUE_BACK", message)
+
+            startActivity(secondActivityIntent)
+
+        }
 
     }
 }
