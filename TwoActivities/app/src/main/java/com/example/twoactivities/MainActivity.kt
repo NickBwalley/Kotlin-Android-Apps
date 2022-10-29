053528959de737fe6ef8281188e86614ba17d367
NickBwalley <nickbwalley@gmail.com>
2022-10-29 23:33:00 +0300

    full functionality, works

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.twoactivities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,8 +14,18 @@ class MainActivity : AppCompatActivity() {
         // inflate the UI -- is transforming the XML layout into java/kotlin objects to work with
         setContentView(R.layout.activity_main)
 
+        // make the text_header2 disappear on initial render
+        var myview: TextView =  findViewById(R.id.text_header2)
+        myview.visibility = View.INVISIBLE
+
         // DISPLAY MESSAGE BACK TO THE MAIN ACTIVITY VIEW.
         val message = intent.getStringExtra("SEND_VALUE_BACK")
+        if(message == null){
+            myview.visibility = View.INVISIBLE
+        } else{
+            myview.visibility = View.VISIBLE
+        }
+
 
         var textView: TextView = findViewById(R.id.text_message2)
 
